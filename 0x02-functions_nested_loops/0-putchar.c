30ae0e340254f08991b98980b41c1d6487c4b069
D9ice <charidzadanai@gmail.com>
2022-09-13 03:36:57 -0700

    zero 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,14 +9,14 @@
 */
 int main(void)
 {
-	char text[9] = "_putchar";
-	int i = 0;
+	char school[10] = "_putchar";
+
+	int i;
 
 	for (i = 0; i < 8; i++)
 	{
-		_putchar(text[i]);
+		_putchar(school[i]);
 	}
-	_putchar('\n');
-
+	_putchar(10);
 	return (0);
 }
